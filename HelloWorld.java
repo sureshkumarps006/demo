655e28b107992a6fe62af9f74c63e7abc4c9f2c4
sureshkumarps006 <91891449+sureshkumarps006@users.noreply.github.com>
2022-12-13 16:59:41 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -10,7 +10,7 @@ public class HelloWorld {
     	        
               
         
-    System.out.println("Hello")	        
+    System.out.println("Hello");	        
         
   }	        
         
