19999982b6599f2c47805351f977eb94e95588a4
sureshkumarps006 <91891449+sureshkumarps006@users.noreply.github.com>
2022-12-13 16:17:29 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
         
   public static void main(String[] args) {	        
         
-    String currentTime="3:37";	        
+    String currentTime="3:07";	        
         
     System.out.println("The current local time is: " + currentTime);	        
         
