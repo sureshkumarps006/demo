efbe1e18b7841210e0c5bbc319f2d873d37189dd
sureshkumarps006 <91891449+sureshkumarps006@users.noreply.github.com>
2022-12-15 10:52:55 +0530

    Update GFG.java

--- GFG.java
+++ GFG.java
@@ -9,7 +9,7 @@ class GFG {
 		String str= "Geeks", nstr="";
 		char ch;
 		
-	System.out.print("Original word: ");
+	System.out.print("Original word= ");
 	System.out.println("Geeks"); //Example word
 		
 	for (int i=0; i<str.length(); i++)
